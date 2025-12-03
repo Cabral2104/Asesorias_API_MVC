ae48ed09868e631d92b40e87ca5fbe4565efafac
cabral2104 <valdezemilio50@gmail.com>
2025-12-03 11:53:28 -0600

    Cambios en corrección de tabla en base de datos.

---
+++ Migrations/20251203175001_RemoverTablaDto.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Asesorias_API_MVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoverTablaDto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AsesorRatingDto");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AsesorRatingDto",
+                columns: table => new
+                {
+                    AsesorId = table.Column<int>(type: "int", nullable: false),
+                    IngresosGenerados = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    NombreAsesor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RatingPromedio = table.Column<double>(type: "float", nullable: false),
+                    TotalCalificaciones = table.Column<int>(type: "int", nullable: false),
+                    TotalCursos = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                });
+        }
+    }
+}
